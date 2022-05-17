35a002c0095ec5af25c27f0d4b3b601269c44e37
Boaz Arsav <9004955@student.tcrmbo.nl>
2022-05-17 12:36:46 +0200

    opdracht onchange

--- app.js
+++ app.js
@@ -1,23 +1,40 @@
 let p = document.getElementById("output");
 let name;
 let country;
+let nameChange = document.getElementById("nameChange");
+let countryChange = document.getElementById("countryChange");
 
-// dit is de name als je het invoert
-const nameChanged = function(event){
+nameChange.addEventListener("change", function(){
     name = event.target.value;
     changeInput();
-}
+})
 
-// dit is de keuze van je land
-const countryChange = function(event){
+countryChange.addEventListener("change", function(){
     country = event.target.value;
     changeInput();
-}
+});
 
-
-// dit laat beide opties zien in text vorm
-const changeInput =  function(){
+const changeInput = function() {
     const text = `Je heet ${name} en je komt uit ${country}`; 
     p.textContent = text;
 }
+
+// // dit is de name als je het invoert
+// const nameChanged = function(event){
+//     name = event.target.value;
+//     changeInput();
+// }
+
+// // dit is de keuze van je land
+// const countryChange = function(event){
+//     country = event.target.value;
+//     changeInput();
+// }
+
+
+// // dit laat beide opties zien in text vorm
+// const changeInput =  function(){
+//     const text = `Je heet ${name} en je komt uit ${country}`; 
+//     p.textContent = text;
+// }
 
